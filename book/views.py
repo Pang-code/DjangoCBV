9965d293f5f7b02ce98f91e43a5d868b001a17d6
pcw <m13421653181@163.com>
2024-03-07 23:30:11 +0800

    feat: 初始化

---
+++ book/views.py
@@ -0,0 +1,36 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+
+
+def book(request):
+    """
+    fbv
+    :param request:
+    :return:
+    """
+    if request.method == "GET":
+        return HttpResponse("get")
+    else:
+        return HttpResponse("other method")
+
+
+class BookView(View):
+    """
+    cbv基础写法
+    """
+
+    def get(self, request):
+        return HttpResponse("get")
+
+    def post(self, request):
+        return HttpResponse("post")
+
+    def put(self, request):
+        return HttpResponse("put")
+
+    def delete(self, request):
+        return HttpResponse("del")
